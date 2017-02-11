9f4ba745abd8383ded5a25125bb7ae19caeebe75
Aaron Sarazan <aaron@levelmoney.com>
2017-02-10 22:31:57 -0800

    flash tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -20,4 +20,23 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
       assert_select 'form.new_user ul li'
   end
 
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "password",
+          password_confirmation: "password"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert-success'
+    assert flash[:success]
+    assert_not flash[:failure]
+  end
+
 end
